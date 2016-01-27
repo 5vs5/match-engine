ca9eede1ef3698a7f9f4410cc31fceac78ec63c1
paxpelus <panagiotis.iatrou@gmail.com>
2016-01-27 18:05:34 +0000

    Misc data changes

--- index.js
+++ index.js
@@ -7,14 +7,11 @@ console.log("Application started on port 5555...");
 
 
 io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+  console.log("#New user started viwing the match");
 });
 
 
 
 var match = require('./lib/match');
 
-match.initialize();
+match.initialize(io);
